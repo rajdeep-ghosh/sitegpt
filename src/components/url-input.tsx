13c8cc0b94af952421d1605bf5ab2632f3ff9012
Rajdeep Ghosh <57666162+rajdeep-ghosh@users.noreply.github.com>
2024-12-02 23:59:37 +0530

    submit form when pressed ENTER key in url input

--- src/components/url-input.tsx
+++ src/components/url-input.tsx
@@ -33,12 +33,20 @@ export default function URLInput() {
     }
   }
 
+  function handleKeyDown(e: React.KeyboardEvent<HTMLTextAreaElement>) {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      handleSubmit();
+    }
+  }
+
   return (
     <div className='flex w-full max-w-xl gap-3 rounded-xl border-[0.5px] border-gray-400/25 bg-muted p-3 transition-colors duration-200 focus-within:border-gray-400/50 hover:border-gray-400/50'>
       <Textarea
         ref={textareaRef}
         value={content}
         onChange={(e) => setContent(e.target.value)}
+        onKeyDown={handleKeyDown}
         placeholder='Enter a url...'
         className='max-h-60 min-h-20 resize-none overflow-hidden p-0 text-sm focus-visible:ring-0 md:text-base'
       />
