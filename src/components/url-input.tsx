b8d8376096d7b5024c5e5116ff4ef0abf376d800
Rajdeep Ghosh <57666162+rajdeep-ghosh@users.noreply.github.com>
2024-12-02 23:57:18 +0530

    minor changes

--- src/components/url-input.tsx
+++ src/components/url-input.tsx
@@ -37,6 +37,7 @@ export default function URLInput() {
     <div className='flex w-full max-w-xl gap-3 rounded-xl border-[0.5px] border-gray-400/25 bg-muted p-3 transition-colors duration-200 focus-within:border-gray-400/50 hover:border-gray-400/50'>
       <Textarea
         ref={textareaRef}
+        value={content}
         onChange={(e) => setContent(e.target.value)}
         placeholder='Enter a url...'
         className='max-h-60 min-h-20 resize-none overflow-hidden p-0 text-sm focus-visible:ring-0 md:text-base'
