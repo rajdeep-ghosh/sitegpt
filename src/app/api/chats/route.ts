c852e493dcb67f5c9165542096837e11c099d0fb
Rajdeep Ghosh <57666162+rajdeep-ghosh@users.noreply.github.com>
2024-12-03 23:59:41 +0530

    create api to index knowledge source and store all indexed sources into db

---
+++ src/app/api/chats/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+import { eq } from 'drizzle-orm';
+
+import db from '@/lib/db';
+import { indexedUrlsTable } from '@/lib/db/schema';
+import { ragChat } from '@/lib/rag-chat';
+
+import type { NextRequest } from 'next/server';
+
+export async function POST(req: NextRequest) {
+  const body = (await req.json()) as { knowledge_src: string };
+
+  const isURLIndexed = await db.query.indexedUrlsTable.findFirst({
+    where: eq(indexedUrlsTable.url, body.knowledge_src),
+    columns: { id: true }
+  });
+
+  if (!!isURLIndexed === false) {
+    await ragChat.context.add({
+      type: 'html',
+      source: body.knowledge_src,
+      config: { chunkOverlap: 50, chunkSize: 200 }
+    });
+
+    await db.insert(indexedUrlsTable).values({ url: body.knowledge_src });
+  }
+
+  return NextResponse.json('ok');
+}
