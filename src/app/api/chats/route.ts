38bf16b2a138d1c349ed12823a1c7a936a70c788
Rajdeep Ghosh <57666162+rajdeep-ghosh@users.noreply.github.com>
2024-12-08 13:21:58 +0530

    handle error

--- src/app/api/chats/route.ts
+++ src/app/api/chats/route.ts
@@ -8,22 +8,29 @@ import { ragChat } from '@/lib/rag-chat';
 import type { NextRequest } from 'next/server';
 
 export async function POST(req: NextRequest) {
-  const body = (await req.json()) as { knowledge_src: string };
+  try {
+    const body = (await req.json()) as { knowledge_src: string };
 
-  const isURLIndexed = await db.query.indexedUrlsTable.findFirst({
-    where: eq(indexedUrlsTable.url, body.knowledge_src),
-    columns: { id: true }
-  });
-
-  if (!!isURLIndexed === false) {
-    await ragChat.context.add({
-      type: 'html',
-      source: body.knowledge_src,
-      config: { chunkOverlap: 50, chunkSize: 200 }
+    const isURLIndexed = await db.query.indexedUrlsTable.findFirst({
+      where: eq(indexedUrlsTable.url, body.knowledge_src),
+      columns: { id: true }
     });
 
-    await db.insert(indexedUrlsTable).values({ url: body.knowledge_src });
-  }
+    if (!!isURLIndexed === false) {
+      await ragChat.context.add({
+        type: 'html',
+        source: body.knowledge_src,
+        config: { chunkOverlap: 50, chunkSize: 200 }
+      });
 
-  return NextResponse.json('ok');
+      await db.insert(indexedUrlsTable).values({ url: body.knowledge_src });
+    }
+
+    return NextResponse.json('ok');
+  } catch (err) {
+    if (err instanceof Error) {
+      return NextResponse.json(err.message, { status: 500 });
+    }
+    return NextResponse.json('Internal Server Error', { status: 500 });
+  }
 }
