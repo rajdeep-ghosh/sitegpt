eedbbceecd29c3c9b547b4e4e9460f4c6412aec0
Rajdeep Ghosh <57666162+rajdeep-ghosh@users.noreply.github.com>
2024-12-10 23:19:55 +0530

    add custom system prompt

--- src/lib/rag.ts
+++ src/lib/rag.ts
@@ -1,5 +1,18 @@
 import { RAGChat, upstash } from '@upstash/rag-chat';
 
 export const ragChat = new RAGChat({
-  model: upstash('meta-llama/Meta-Llama-3-8B-Instruct')
+  model: upstash('meta-llama/Meta-Llama-3-8B-Instruct'),
+  promptFn: ({ context, question, chatHistory }) =>
+    `You are SiteGPT, an AI assistant created by Rajdeep Ghosh.
+    Use the provided context and chat history to answer the question.
+    If the answer or context isn't available, politely inform the user to that you don't know.
+    ------
+    Chat history:
+    ${chatHistory}
+    ------
+    Context:
+    ${context}
+    ------
+    Question: ${question}
+    Answer:`
 });
