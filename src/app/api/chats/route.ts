433f216e49cb6b565ff3eed0a4cc661f2477c6fc
Rajdeep Ghosh <57666162+rajdeep-ghosh@users.noreply.github.com>
2024-12-12 20:22:13 +0530

    return api success and error states

--- src/app/api/chats/route.ts
+++ src/app/api/chats/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from 'next/server';
 import { auth } from '@clerk/nextjs/server';
 import { eq } from 'drizzle-orm';
+import { generateErrorMessage } from 'zod-error';
 
+import { createChatReqSchema } from '@/lib/api/schema/chats';
 import db from '@/lib/db';
 import { chatsTable, indexedUrlsTable } from '@/lib/db/schema';
 import { ragChat } from '@/lib/rag';
@@ -13,50 +15,80 @@ export async function POST(req: NextRequest) {
   const { userId } = await auth();
 
   if (!userId) {
-    return NextResponse.json('Unauthorized', { status: 401 });
+    return NextResponse.json(
+      { status: 'error', message: 'Unauthorized' },
+      { status: 401 }
+    );
   }
 
   try {
-    const body = (await req.json()) as { knowledge_src: string };
+    const body = createChatReqSchema.safeParse(await req.json());
 
-    const isURLSupported = await isHTML(body.knowledge_src);
-    if (!isURLSupported) {
+    if (!body.success) {
+      const errMsg = generateErrorMessage(body.error.issues, {
+        maxErrors: 1,
+        delimiter: { component: ': ' },
+        code: { enabled: false },
+        path: { enabled: true, type: 'objectNotation', label: '' },
+        message: { enabled: true, label: '' }
+      });
       return NextResponse.json(
-        'The provided URL does not point to an HTML document',
+        { status: 'error', message: errMsg },
         { status: 400 }
       );
     }
 
+    const isURLSupported = await isHTML(body.data.knowledge_src);
+    if (!isURLSupported) {
+      return NextResponse.json(
+        {
+          status: 'error',
+          message: 'The provided URL does not point to an HTML document'
+        },
+        { status: 415 }
+      );
+    }
+
     const isURLIndexed = await db.query.indexedUrlsTable.findFirst({
-      where: eq(indexedUrlsTable.url, body.knowledge_src),
+      where: eq(indexedUrlsTable.url, body.data.knowledge_src),
       columns: { id: true }
     });
 
     if (!!isURLIndexed === false) {
       await ragChat.context.add({
         type: 'html',
-        source: body.knowledge_src,
+        source: body.data.knowledge_src,
         config: { chunkOverlap: 50, chunkSize: 200 }
       });
-
-      await db.insert(indexedUrlsTable).values({ url: body.knowledge_src });
+      await db
+        .insert(indexedUrlsTable)
+        .values({ url: body.data.knowledge_src });
     }
 
-    const siteTitle = await extractSiteTitle(body.knowledge_src);
+    const siteTitle = await extractSiteTitle(body.data.knowledge_src);
     const [newChat] = await db
       .insert(chatsTable)
       .values({
         userId,
         siteTitle,
-        siteUrl: body.knowledge_src
+        siteUrl: body.data.knowledge_src
       })
       .returning();
 
-    return NextResponse.json(newChat, { status: 201 });
+    return NextResponse.json(
+      { status: 'success', data: newChat },
+      { status: 201 }
+    );
   } catch (err) {
     if (err instanceof Error) {
-      return NextResponse.json(err.message, { status: 500 });
+      return NextResponse.json(
+        { status: 'error', message: err.message },
+        { status: 500 }
+      );
     }
-    return NextResponse.json('Internal Server Error', { status: 500 });
+    return NextResponse.json(
+      { status: 'error', message: 'Internal Server Error' },
+      { status: 500 }
+    );
   }
 }
---
+++ src/lib/api/schema/chats.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+/*
+  match all urls according to follwing conditions
+  - must start with either http:// or https://
+  - domain and subdomain with allowed characters (alphanumeric, hyphen)
+  - requires at least 2 alphabetic characters for the TLD
+  - optional path
+  - optional query parameters
+*/
+const urlRegex =
+  /^(https?:\/\/)([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}(\/[^\s]*)?(\?[a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=]*)?$/g;
+
+export const createChatReqSchema = z.object({
+  knowledge_src: z.string().regex(urlRegex, 'Invalid URL')
+});
