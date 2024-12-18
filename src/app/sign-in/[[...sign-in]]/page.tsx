74d86136b5ed88256352b0797776185f85e41404
Rajdeep Ghosh <57666162+rajdeep-ghosh@users.noreply.github.com>
2024-12-19 04:48:01 +0530

    change page names

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,6 @@
 import { SignIn } from '@clerk/nextjs';
 
-export default function LoginPage() {
+export default function SignInPage() {
   return (
     <main className='grid h-screen place-items-center'>
       <SignIn />
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,6 @@
 import { SignUp } from '@clerk/nextjs';
 
-export default function LoginPage() {
+export default function SignUpPage() {
   return (
     <main className='grid h-screen place-items-center'>
       <SignUp />
