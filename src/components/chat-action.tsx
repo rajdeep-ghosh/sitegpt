703288373966441b14e184f4093104bc79416ae8
Rajdeep Ghosh <57666162+rajdeep-ghosh@users.noreply.github.com>
2024-12-21 07:10:40 +0530

    add chat rename

--- src/components/chat-action.tsx
+++ src/components/chat-action.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { Check, Ellipsis, Link } from 'lucide-react';
+import { Check, Ellipsis, Link, PenLine } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
 import {
@@ -19,6 +19,8 @@ import {
   SidebarMenuItem
 } from '@/components/ui/sidebar';
 
+import ChatRename from './chat-rename';
+
 import type { Chat } from '@/types';
 
 type ChatActionProps = {
@@ -27,54 +29,71 @@ type ChatActionProps = {
 
 export default function ChatAction({ chat }: ChatActionProps) {
   const [copiedToClipboard, setCopiedToClipboard] = useState(false);
+  const [openChatRename, setOpenChatRename] = useState(false);
 
   return (
-    <div className='flex items-center gap-2 text-sm'>
-      <div className='hidden font-medium text-muted-foreground md:inline-block'>
-        {new Date(chat.createdAt).toLocaleDateString('en', {
-          month: 'short',
-          day: 'numeric'
-        })}
-      </div>
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button
-            variant='ghost'
-            size='icon'
-            className='size-7 data-[state=open]:bg-accent'
+    <>
+      <div className='flex items-center gap-2 text-sm'>
+        <div className='hidden font-medium text-muted-foreground md:inline-block'>
+          {new Date(chat.createdAt).toLocaleDateString('en', {
+            month: 'short',
+            day: 'numeric'
+          })}
+        </div>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              variant='ghost'
+              size='icon'
+              className='size-7 data-[state=open]:bg-accent'
+            >
+              <Ellipsis />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent
+            align='end'
+            className='w-56 overflow-hidden rounded-lg p-0'
           >
-            <Ellipsis />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent
-          align='end'
-          className='w-56 overflow-hidden rounded-lg p-0'
-        >
-          <Sidebar collapsible='none'>
-            <SidebarContent>
-              <SidebarGroup className='p-1.5'>
-                <SidebarGroupContent className='gap-0'>
-                  <SidebarMenu>
-                    <SidebarMenuItem>
-                      <SidebarMenuButton
-                        onClick={() => {
-                          setCopiedToClipboard(true);
-                          void navigator.clipboard.writeText(chat.siteUrl);
-                          setTimeout(() => setCopiedToClipboard(false), 1500);
-                        }}
-                        className='[&>svg]:text-muted-foreground [&>svg]:transition-colors [&>svg]:[shape-rendering:crispEdges] [&>svg]:hover:text-current'
-                      >
-                        {copiedToClipboard ? <Check /> : <Link />}
-                        <span>Copy Source Link</span>
-                      </SidebarMenuButton>
-                    </SidebarMenuItem>
-                  </SidebarMenu>
-                </SidebarGroupContent>
-              </SidebarGroup>
-            </SidebarContent>
-          </Sidebar>
-        </PopoverContent>
-      </Popover>
-    </div>
+            <Sidebar collapsible='none'>
+              <SidebarContent>
+                <SidebarGroup className='p-1.5'>
+                  <SidebarGroupContent className='gap-0'>
+                    <SidebarMenu>
+                      <SidebarMenuItem>
+                        <SidebarMenuButton
+                          onClick={() => {
+                            setCopiedToClipboard(true);
+                            void navigator.clipboard.writeText(chat.siteUrl);
+                            setTimeout(() => setCopiedToClipboard(false), 1500);
+                          }}
+                          className='[&>svg]:text-muted-foreground [&>svg]:transition-colors [&>svg]:[shape-rendering:crispEdges] [&>svg]:hover:text-current'
+                        >
+                          {copiedToClipboard ? <Check /> : <Link />}
+                          <span>Copy Source Link</span>
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>
+                      <SidebarMenuItem>
+                        <SidebarMenuButton
+                          onClick={() => setOpenChatRename(true)}
+                          className='[&>svg]:text-muted-foreground [&>svg]:transition-colors [&>svg]:[shape-rendering:crispEdges] [&>svg]:hover:text-current'
+                        >
+                          <PenLine />
+                          <span>Rename</span>
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>
+                    </SidebarMenu>
+                  </SidebarGroupContent>
+                </SidebarGroup>
+              </SidebarContent>
+            </Sidebar>
+          </PopoverContent>
+        </Popover>
+      </div>
+      <ChatRename
+        open={openChatRename}
+        onOpenChange={setOpenChatRename}
+        chat={chat}
+      />
+    </>
   );
 }
---
+++ src/components/chat-rename.tsx
@@ -0,0 +1,121 @@
+import { useState } from 'react';
+import { Loader2 } from 'lucide-react';
+import { mutate } from 'swr';
+
+import { useIsMobile } from '@/hooks/use-mobile';
+import { toast } from '@/hooks/use-toast';
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle
+} from '@/components/ui/dialog';
+import {
+  Drawer,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle
+} from '@/components/ui/drawer';
+import { Input } from '@/components/ui/input';
+
+import type { Chat } from '@/types';
+
+type ChatRenameProps = {
+  open: boolean;
+  onOpenChange: React.Dispatch<React.SetStateAction<boolean>>;
+  chat: Chat['data'];
+};
+
+export default function ChatRename({
+  open,
+  onOpenChange,
+  chat
+}: ChatRenameProps) {
+  const isMobile = useIsMobile();
+
+  const Comp = isMobile ? Drawer : Dialog;
+  const CompContent = isMobile ? DrawerContent : DialogContent;
+  const CompHeader = isMobile ? DrawerHeader : DialogHeader;
+  const CompTitle = isMobile ? DrawerTitle : DialogTitle;
+  const CompFooter = isMobile ? DrawerFooter : DialogFooter;
+
+  const [title, setTitle] = useState(chat.siteTitle);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  async function handleSubmit() {
+    try {
+      setIsSubmitting(true);
+
+      const updateChatRes = await fetch(`/api/chats/${chat.id}`, {
+        method: 'PATCH',
+        body: JSON.stringify({ title })
+      });
+      const body = (await updateChatRes.json()) as
+        | { status: 'error'; message: string }
+        | Chat;
+
+      switch (body.status) {
+        case 'error':
+          throw new Error(body.message);
+
+        case 'success':
+          onOpenChange(false);
+          mutate('/api/chats');
+          toast({
+            title: 'Success',
+            description: 'Chat Renamed'
+          });
+      }
+    } catch (err) {
+      if (err instanceof Error) {
+        toast({
+          variant: 'destructive',
+          title: 'Uh oh! Something went wrong.',
+          description: err.message
+        });
+      } else {
+        toast({
+          variant: 'destructive',
+          title: 'Uh oh! Something went wrong.',
+          description: 'Please try again!'
+        });
+      }
+      console.error(err);
+    } finally {
+      setIsSubmitting(false);
+    }
+  }
+
+  return (
+    <Comp open={open} onOpenChange={onOpenChange}>
+      <CompContent>
+        <CompHeader>
+          <CompTitle>Rename Chat</CompTitle>
+        </CompHeader>
+        <div className='p-4 md:p-0'>
+          <label htmlFor='newChatTitle' className='sr-only'>
+            New chat title
+          </label>
+          <Input
+            id='newChatTitle'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <CompFooter>
+          <Button
+            size='sm'
+            disabled={isSubmitting}
+            onClick={handleSubmit}
+            className='rounded-lg text-sm'
+          >
+            {isSubmitting ? <Loader2 className='animate-spin' /> : 'Save'}
+          </Button>
+        </CompFooter>
+      </CompContent>
+    </Comp>
+  );
+}
