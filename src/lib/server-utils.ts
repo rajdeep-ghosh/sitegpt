7ff8ba77a2cecb08dd343a974ce33833fb0e3516
Rajdeep Ghosh <57666162+rajdeep-ghosh@users.noreply.github.com>
2024-12-31 22:17:21 +0530

    fix functionality to check if current env is server or not

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -4,7 +4,21 @@ import { generateErrorMessage } from 'zod-error';
 import type { ZodIssue } from 'zod';
 import type { ErrorMessageOptions } from 'zod-error';
 
-export const isServer = typeof window === 'undefined' ? false : true;
+export const isServer = typeof window === 'undefined' ? true : false;
+
+export function validationErrorMessage(
+  issues: ZodIssue[],
+  options?: ErrorMessageOptions
+) {
+  return generateErrorMessage(issues, {
+    maxErrors: 1,
+    delimiter: { component: ': ' },
+    code: { enabled: false },
+    path: { enabled: true, type: 'objectNotation', label: '' },
+    message: { enabled: true, label: '' },
+    ...options
+  });
+}
 
 export async function isHTML(url: string) {
   const response = await fetch(url, { method: 'HEAD' });
@@ -27,18 +41,4 @@ export async function extractSiteTitle(url: string) {
   const { document } = new JSDOM(html).window;
   const title = document.querySelector('title')?.textContent ?? 'Untitled';
   return title;
-}
-
-export function validationErrorMessage(
-  issues: ZodIssue[],
-  options?: ErrorMessageOptions
-) {
-  return generateErrorMessage(issues, {
-    maxErrors: 1,
-    delimiter: { component: ': ' },
-    code: { enabled: false },
-    path: { enabled: true, type: 'objectNotation', label: '' },
-    message: { enabled: true, label: '' },
-    ...options
-  });
 }
