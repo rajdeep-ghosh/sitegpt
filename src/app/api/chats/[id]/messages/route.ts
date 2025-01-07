b770b19a45a15798d702348d9fb17ea36d2aafe8
Rajdeep Ghosh <57666162+rajdeep-ghosh@users.noreply.github.com>
2025-01-07 21:01:03 +0530

    create api to get chat messages

---
+++ src/app/api/chats/[id]/messages/route.ts
@@ -0,0 +1,57 @@
+import { NextResponse } from 'next/server';
+import { auth } from '@clerk/nextjs/server';
+import { and, eq } from 'drizzle-orm';
+
+import db from '@/lib/db';
+import { chatsTable } from '@/lib/db/schema';
+import { ragChat } from '@/lib/rag';
+
+import type { NextRequest } from 'next/server';
+
+type RouteProps = {
+  params: {
+    id: string;
+  };
+};
+
+export async function GET(req: NextRequest, { params }: RouteProps) {
+  const { userId: _userId } = await auth();
+  const userId = _userId ?? req.headers.get('x-userid');
+
+  if (!userId) {
+    return NextResponse.json(
+      { status: 'error', message: 'Unauthorized' },
+      { status: 401 }
+    );
+  }
+
+  try {
+    const chat = await db.query.chatsTable.findFirst({
+      where: and(eq(chatsTable.userId, userId), eq(chatsTable.id, params.id))
+    });
+
+    if (!chat) {
+      return NextResponse.json(
+        { status: 'error', message: 'Not Found' },
+        { status: 404 }
+      );
+    }
+
+    const messages = await ragChat.history.getMessages({
+      sessionId: params.id
+    });
+
+    return NextResponse.json({ status: 'success', data: messages });
+  } catch (err) {
+    if (err instanceof Error) {
+      return NextResponse.json(
+        { status: 'error', message: err.message },
+        { status: 500 }
+      );
+    }
+    return NextResponse.json(
+      { status: 'error', message: 'Internal Server Error' },
+      { status: 500 }
+    );
+  }
+}
