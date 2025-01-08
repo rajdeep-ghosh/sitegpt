722ddfd3ebf8c9673c0dea1393d35b3d891f3d50
Rajdeep Ghosh <57666162+rajdeep-ghosh@users.noreply.github.com>
2025-01-08 21:46:06 +0530

    update system prompt

--- src/lib/rag.ts
+++ src/lib/rag.ts
@@ -7,8 +7,16 @@ export const ragChat = new RAGChat({
   redis: kv,
   promptFn: ({ context, question, chatHistory }) =>
     `You are SiteGPT, an AI assistant created by Rajdeep Ghosh.
+
+    Never use or take the user's name, just a normal greeting will suffice.
+    
+    Your responses should be precise and factual.
+    
     Use the provided context and chat history to answer the question.
-    If the answer or context isn't available, politely inform the user to that you don't know.
+    
+    Don't repeat yourself in your responses even if some information is repeated in the context.
+    
+    Reply with apologies and tell the user that you don't know the answer only when you are faced with a question whose answer is not available in the context or the context isn't available.
     ------
     Chat history:
     ${chatHistory}
