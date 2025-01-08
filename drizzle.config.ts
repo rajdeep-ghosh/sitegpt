9c32d80623a824623fa8b35c7afa415ee6b68bb6
Rajdeep Ghosh <57666162+rajdeep-ghosh@users.noreply.github.com>
2025-01-08 20:42:07 +0530

    remove unwanted config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from 'drizzle-kit';
 
-import { env } from '@/lib/env';
-
 export default defineConfig({
   schema: './src/lib/db/schema.ts',
   out: './src/lib/db/migrations',
   dialect: 'postgresql',
-  dbCredentials: {
-    url: env.DB_URL
-  },
   verbose: true,
   strict: true
 });
