6a5272d3703a3d57fb46a52d68a1b0cedb52e344
Rajdeep Ghosh <57666162+rajdeep-ghosh@users.noreply.github.com>
2025-01-13 20:36:37 +0530

    fix scrollbar position

--- src/components/chat-container.tsx
+++ src/components/chat-container.tsx
@@ -24,26 +24,30 @@ export default function ChatContainer({ initialMessages }: ChatContainerProps) {
   });
 
   return (
-    <main className='mx-auto h-[calc(100dvh-60px)] max-w-2xl p-2'>
-      <div className='flex h-full flex-col justify-between gap-4 px-4'>
-        <div className='scrollbar grow space-y-4 overflow-y-auto pr-1'>
-          {messages.length > 0 ? (
-            messages.map((message) => (
-              <ChatMessage key={message.id} message={message} />
-            ))
-          ) : (
-            <ChatGreeting />
-          )}
+    <main className='h-[calc(100dvh-60px)] py-2'>
+      <div className='flex h-full flex-col justify-between gap-4'>
+        <div className='scrollbar grow overflow-y-auto'>
+          <div className='mx-auto h-full max-w-2xl space-y-4 px-6'>
+            {messages.length > 0 ? (
+              messages.map((message) => (
+                <ChatMessage key={message.id} message={message} />
+              ))
+            ) : (
+              <ChatGreeting />
+            )}
+          </div>
         </div>
-        <div className='flex flex-col items-center gap-2'>
-          <ChatInput
-            input={input}
-            handleInputChange={handleInputChange}
-            handleSubmit={handleSubmit}
-          />
-          <p className='text-center text-xs text-muted-foreground'>
-            SiteGPT can make mistakes. Check important info.
-          </p>
+        <div className='mx-auto w-full max-w-2xl px-4'>
+          <div className='flex flex-col items-center gap-2'>
+            <ChatInput
+              input={input}
+              handleInputChange={handleInputChange}
+              handleSubmit={handleSubmit}
+            />
+            <p className='text-center text-xs text-muted-foreground'>
+              SiteGPT can make mistakes. Check important info.
+            </p>
+          </div>
         </div>
       </div>
     </main>
